eb2bfc2887107605bd3a671f6e9e53e42a34f0f6
djimenezga11 <imaginario96@gmail.com>
2017-05-31 16:19:23 +0200

    Add files via upload

---
+++ Golpear.py
@@ -0,0 +1,14 @@
+impor random 
+
+class Golpear(object):
+	def __init__(self):
+		self.quita = 0
+		self.nombre = "Golpear"
+		
+	def devolverAtaque(self,origen):
+		if origen.dano<5:
+			return 0
+		else:
+			self.quita = random.randrange(12,16)+origen.fuerza
+			origen.dano += -5
+		return self.quita
